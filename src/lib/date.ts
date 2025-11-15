3ef5e297e0867b7233856cc995488451bd23cfac
kanaru-ssk <kanaru.ssk@gmail.com>
2025-11-15 19:38:22 +0900

    refactor formatDate func

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,16 +1,20 @@
 type Template = "YYYY-MM-DD" | "YYYY/MM/DD" | "YYYY年M月";
 
 export function formatDate(date: Date, template: Template = "YYYY-MM-DD") {
+  const yyyy = date.getFullYear();
+  const m = date.getMonth() + 1;
+  const mm = String(m).padStart(2, "0");
+  const d = date.getDate();
+  const dd = String(d).padStart(2, "0");
+
   if (template === "YYYY年M月") {
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    return `${year}年${month}月`;
+    return `${yyyy}年${m}月`;
   }
 
   if (template === "YYYY/MM/DD") {
-    return date.toLocaleDateString("ja-JP");
+    return `${yyyy}/${mm}/${dd}`;
   }
 
-  // default: "YYYY-MM-DD"
-  return date.toISOString().split("T")[0];
+  // YYYY-MM-DD
+  return `${yyyy}-${mm}-${dd}`;
 }
